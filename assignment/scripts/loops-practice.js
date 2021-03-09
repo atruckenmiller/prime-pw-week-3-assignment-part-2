dd29fa577430cdc000fb1c9db9ec9645bafac063
Andrew <adtruckenmiller@gmail.com>
2021-03-08 19:40:20 -0600

    Completed Assignment

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -26,16 +26,26 @@ for (let i=3; i<6; i++) {
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for ( let i = 2; i <= 10; i++){
+  if(i % 2 == 0){
+    console.log('even numbers:',i + 0);
+  }
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for ( let i = 5; i >= 0; i--){
+  if(i % 1 == 0){
+    console.log('count backwards', i - 0);
+  }
+}
 
 // 2. For of loops
 console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
+
 console.log('Some stars:');
 
 
@@ -46,12 +56,15 @@ console.log('---- 3. While loop ----');
 console.log('Some stars using while:');
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
-console.log('count from 0 to 5');
-let x = 0;
-let y = 0;
-while (x < 5){
-  x++;
-  y += x;
+console.log('Count from 0 to 5');
+let i = 0;
+const max = 6;
+// while loop
+while( i < max ){
+  console.log('In while loop. i:', i );
+  i++;
 }
+
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
